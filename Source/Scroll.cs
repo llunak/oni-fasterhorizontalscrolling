b4816c772b96c8a66fd0274d0d3226df2e13667f
Luboš Luňák <l.lunak@centrum.cz>
2023-04-15 21:27:21 +0200

    make horizontal scrolling faster

---
+++ Source/Scroll.cs
@@ -0,0 +1,15 @@
+using HarmonyLib;
+
+namespace DeliveryTemperatureLimit
+{
+    [HarmonyPatch(typeof(KScrollRect))]
+    public class KScreenRect_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(MethodType.Constructor)]
+        public static void ctor(ref float ___horizontalScrollInertiaScale)
+        {
+            ___horizontalScrollInertiaScale *= 4;
+        }
+    }
+}
